755bf708d690e1cda8bc46fc3fc063adc41b9adf
Ectogen <69585636+Electogenius@users.noreply.github.com>
2021-07-13 12:53:36 +0530

    hmm

--- api/index.js
+++ api/index.js
@@ -1,25 +1,11 @@
 let app = require("express")()
-var a=0 //index
-/*
+let fs=require("fs")
+var a=Number(fs.readFileSync("./count.txt","utf-8"))
 app.get("/api",(req, res)=>{
     a++
-    let eth="th",e=String(a)
-    if(!(/^\d*1\d$/.test(e))){
-    if(e.endsWith("1"))eth="st"
-    if(e.endsWith("2"))eth="nd"
-    if(e.endsWith("3"))eth="rd"
-    }
-    res.write(e + eth)
-    res.end()
-})
-*/
-app.get("/api",(req, res)=>{
-    a++
+    fs.writeFileSync("./count.txt",a)
     res.write(String(a))
     res.end()
 })
-
-app.get("/teapot",(_,r)=>r.send(418))
-
 app.get
 app.listen(3900)
