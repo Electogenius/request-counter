6134eeb9a66302c57861d09f76cce5ed93b941e1
Ectogen <69585636+Electogenius@users.noreply.github.com>
2021-07-14 11:26:25 +0530

    Added the http request
    Hope I did this right,

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 let app = require("express")()
 let fs=require("fs")
-var a=Number(fs.readFileSync("./count.txt","utf-8"))
 app.get("/api",(req, res)=>{
-    a++
-    fs.writeFileSync("./count.txt",a)
-    res.write(String(a))
-    res.end()
+    //<smart type=code author=waff>
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'https://api.github.com/repos/liimee/request-counter/actions/workflows/rickroll.yml/dispatches');
+    xhr.setRequestHeader("Authorization", "Bearer sometokenhere");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+      }
+    };
+    xhr.send('{"ref": "main"}');
+    //</smart>
 })
-app.get
 app.listen(3900)
